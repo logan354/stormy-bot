f6c53d4241a896475e348d935c1cf764a8e0a7e4
Block354 <75645441+Block354@users.noreply.github.com>
2023-04-21 14:01:38 +1200

    feat: update guild systems

--- structures/guildSystems/forecast-system.js
+++ structures/guildSystems/forecast-system.js
@@ -1,41 +1,33 @@
 const { Client } = require("discord.js");
 const { default: fetch } = require("node-fetch");
-const { baseForecastTitle, baseForecast, baseLocalObservation } = require("../baseFormats");
+const { baseForecastTitle, baseForecast } = require("../baseFormats");
 const { apiBaseURL, apiOptions, guildLocation, guildChannels } = require("../../utils/constants");
 
 /**
  * @param {Client} client 
  */
 module.exports = (client) => {
-    function run() {
-        const time = new Date().toLocaleTimeString();
+    async function run() {
+        const d = new Date();
 
-        if (time === "6:00:00 PM") {
-            //-------"6:00:00 AM NZST"
-            localForecastSystem();
-        }
+        // NZST = UTC+12
+        if (d.getUTCHours() !== 18 && d.getMinutes() !== 0) return;
 
-        if (time === "3:00:00 PM" || time === "6:00:00 PM" || time === "9:00:00 PM" || time === "12:00:00 AM" || time === "3:00:00 AM" || time === "6:00:00 AM" || time === "9:00:00 AM" || time === "12:00:00 PM") {
-            //-------"3:00:00 AM NZDT"--------"6:00:00 AM NZDT"--------"9:00:00 AM NZDT"--------"12:00:00 PM NZDT"--------"3:00:00 PM NZDT"--------"6:00:00 PM NZDT"--------"9:00:00 PM NZDT"--------"12:00:00 AM NZDT"
-            localObservationSystem();
-        }
-    }
-
-    async function localForecastSystem() {
         // Fetch data from MetService API
         try {
             const response = await fetch(apiBaseURL + apiOptions.LOCAL_FORECAST + guildLocation.replace(" ", "-"));
             var data = await response.json();
         } catch (error) {
-            if (e.name === "FetchError" && e.type === "invalid-json") {
+            if (error.name === "FetchError" && error.type === "invalid-json") {
                 return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Invalid location**");
             } else {
                 console.error(error);
-                return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Error(Guild System: Local Forecast System)** `" + error.message + "`");
+                return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Error(Guild System: Forecast System)** `" + error.message + "`");
             }
         }
 
-        let outlook = 1;
+        let outlook = 3;
+
         const finalData = [];
         const charLimit = 2000;
         let k = 0;
@@ -59,87 +51,11 @@ module.exports = (client) => {
 
         // Iterate through formatted data array
         for (let i of finalData) {
-            client.channels.cache.get(guildChannels.DAILY_FORECAST_CHANNEL).send(i)
+            client.channels.cache.get(guildChannels.FORECAST_CHANNEL).send(i)
                 .then(message => message.crosspost())
-                .catch(e => console.error(error));
-        }
-
-        outlook = 3;
-        finalData = [];
-        k = 0;
-        isToday = false;
-
-        for (let i = 0; i < outlook; i++) {
-            if (i === 0) {
-                isToday = true;
-                finalData[k] = baseForecastTitle(data.locationIPS, null, outlook);
-            } else {
-                isToday = false;
-            }
-
-            if (finalData[k].length + baseForecast(data.days[i], isToday).length > charLimit) {
-                k++
-                finalData[k] = baseForecast(data.days[i], isToday);
-            } else {
-                finalData[k] += baseForecast(data.days[i], isToday);
-            }
-        }
-
-        // Iterate through formatted data array
-        for (let i of finalData) {
-            client.channels.cache.get(guildChannels.THREE_DAY_FORECAST_CHANNEL).send(i)
-                .then(message => message.crosspost())
-                .catch(e => console.error(error));
-        }
-
-        outlook = 5;
-        finalData = [];
-        k = 0;
-        isToday = false;
-
-        for (let i = 0; i < outlook; i++) {
-            if (i === 0) {
-                isToday = true;
-                finalData[k] = baseForecastTitle(data.locationIPS, null, outlook);
-            } else {
-                isToday = false;
-            }
-
-            if (finalData[k].length + baseForecast(data.days[i], isToday).length > charLimit) {
-                k++
-                finalData[k] = baseForecast(data.days[i], isToday);
-            } else {
-                finalData[k] += baseForecast(data.days[i], isToday);
-            }
-        }
-
-        // Iterate through formatted data array
-        for (let i of finalData) {
-            client.channels.cache.get(guildChannels.FIVE_DAY_FORECAST_CHANNEL).send(i)
-                .then(message => message.crosspost())
-                .catch(e => console.error(error));
+                .catch(error => console.error(error));
         }
     }
 
-    async function localObservationSystem() {
-        // Fetch data from MetService API
-        try {
-            const response = await fetch(apiBaseURL + apiOptions.LOCAL_OBS + guildLocation.replace(" ", "-"));
-            var data = await response.json();
-        } catch (error) {
-            if (e.name === "FetchError" && e.type === "invalid-json") {
-                return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Invalid location**");
-            } else {
-                console.error(error);
-                return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Error(Guild System: Local Observation System)** `" + error.message + "`");
-            }
-        }
-
-        const embed = baseLocalObservation(data);
-        client.channels.cache.get(guildChannels.THREE_HOUR_OBSERVATION_CHANNEL).send({ embeds: [embed] })
-            .then(message => message.crosspost())
-            .catch(e => console.error(error));
-    }
-
-    setInterval(run, 1000);
+    setInterval(run, 60000);
 }
--- structures/guildSystems/warning-system.js
+++ structures/guildSystems/warning-system.js
@@ -1,53 +1,76 @@
-const { Client } = require("discord.js");
+const { Client, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { default: fetch } = require("node-fetch");
-const { baseWarningTitle, baseWarning } = require("../baseFormats");
-const { apiBaseURL, apiOptions, guildLocation, guildChannels } = require("../../utils/constants");
+const { baseWarning } = require("../baseFormats");
+const { apiBaseURL, apiOptions, capBaseURL, guildLocation, guildChannels } = require("../../utils/constants");
 
 /**
  * @param {Client} client 
  */
 module.exports = (client) => {
-    let perviousWarning = null;
-    
+    let cache = null;
+
     async function run() {
-        // Fetch data from MetService API
+        // Fetch data from MetService CAP Server
         try {
-            const response = await fetch(apiBaseURL + apiOptions.WARNINGS + guildLocation.replace(" ", "-"));
-            var data = await response.json();
+            const response = await fetch(capBaseURL);
+            const xml = await response.text();
+            const rawData = xml2js(xml, { compact: true, spaces: 4 });
+
+            var data = [];
+
+            for (let i = 0; rawData.rss.channel.item ? i < rawData.rss.channel.item.length : i < 0; i++) {
+                const response = await fetch(rawData.rss.channel.item[i].link._text);
+                const xml = await response.text();
+
+                // Filter only warnings & watches for the Hamilton area
+                const formatData = xml2js(xml, { compact: true, spaces: 4 }).alert.info;
+                if (formatData.area.areaDesc._text.toLowercase().includes("hamilton") || formatData.area.areaDesc._text.toLowercase().includes("waikato")) data.push(formatData);
+            }
         } catch (error) {
-            if (e.name === "FetchError" && e.type === "invalid-json") {
-                return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Invalid location**");
-            } else {
-                console.error(error);
-                return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Error(Guild System: Warning System)** `" + error.message + "`");
+            console.error(error);
+            return client.channels.cache.get(guildChannels.SERVER_LOGS_CHANNEL).send(client.emotes.error + " **Error(Guild System: Warning System)** `" + error.message + "`");
+        }
+
+        const page = []
+        const pageSize = 5;
+        let currentPage = 1;
+
+        // Algorithm for sorting data into format data and then pages
+        let j = 0;
+        for (let i = 0; i < data.length; i++) {
+            if (j === 0) page[0] = baseWarning(data[i]);
+            else page[0] += baseWarning(data[i]);
+
+            if (j === pageSize - 1) {
+                page[0] += "..."
+                break;
             }
+            else j++;
         }
 
-        const finalData = [];
-        const charLimit = 2000;
-        let k = 0;
+        const embed = new EmbedBuilder()
+            .setColor("Grey")
+            .setTitle("Warnings & Watches for Hamilton")
+            .setDescription(page.length > 0 ? page[currentPage - 1] : "**No Warnings & Watches**")
+            .setTimestamp();
 
-        for (let i = 0; i < data.warnings.length; i++) {
-            if (i === 0) finalData[k] = "[ @everyone ]\n" + baseWarningTitle(data.locationName);
+        const row = new ActionRowBuilder()
+            .addComponents(
+                [
+                    new ButtonBuilder()
+                        .setLabel("Warnings")
+                        .setURL("https://www.metservice.com/warnings/home")
+                        .setStyle(ButtonStyle.Link),
+                ]
+            );
 
-            if (finalData[k].length + baseWarning(data.warnings[i]).length > charLimit) {
-                k++
-                finalData[k] = baseWarning(data.warnings[i]);
-            } else {
-                finalData[k] += baseWarning(data.warnings[i]);
-            }
+        if (!page.length || page.join() === cache) {
+            return;
+        } else {
+            cache = page.join();
+            client.channels.cache.get(guildChannels.WARNING_CHANNEL).send({ embeds: [embed], components: [row] });
+
         }
-
-        if (!finalData.length || finalData.join() === perviousWarning) return;
-
-        // Iterate through formatted data array
-        for (let i of finalData) {
-            client.channels.cache.get(guildChannels.WARNING_CHANNEL).send(i)
-            .then(message => message.crosspost())
-            .catch(e => console.error(error));
-        }
-
-        perviousWarning = finalData.join();
     }
 
     setInterval(run, 60000);
